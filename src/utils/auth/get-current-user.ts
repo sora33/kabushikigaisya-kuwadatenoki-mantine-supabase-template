8587f2a4e3c47d8af2ff2da4f902fa26568c306f
Hiranuma Shuya(ひらぬま) <shuuuya0616@gmail.com>
2024-10-07 15:38:45 +0900

    メモ化

--- src/utils/auth/get-current-user.ts
+++ src/utils/auth/get-current-user.ts
@@ -1,19 +1,22 @@
 import type { User } from "@supabase/supabase-js";
+import { cache } from "react";
 import { createServerClient } from "~/lib/supabase";
 
-export async function getCurrentUser(): Promise<{
-	isAdmin: boolean;
-	isSignedIn: boolean;
-	currentUser: User | null;
-}> {
-	const supabase = createServerClient();
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
+export const getCurrentUser = cache(
+	async (): Promise<{
+		isAdmin: boolean;
+		isSignedIn: boolean;
+		currentUser: User | null;
+	}> => {
+		const supabase = createServerClient();
+		const {
+			data: { user },
+		} = await supabase.auth.getUser();
 
-	const isAdmin = user?.app_metadata.role === "admin";
-	const isSignedIn = !!user;
-	const currentUser: User | null = user;
+		const isAdmin = user?.app_metadata.role === "admin";
+		const isSignedIn = !!user;
+		const currentUser: User | null = user;
 
-	return { isAdmin, isSignedIn, currentUser };
-}
+		return { isAdmin, isSignedIn, currentUser };
+	},
+);
